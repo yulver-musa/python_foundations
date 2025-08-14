1a01f324a5c7229205477363c9d32c514e4036ef
Yulver <yulver.musa@gmail.com>
2025-08-14 20:41:07 +0200

    Added new file.

---
+++ python_foundations/2.03_deposit_calculator.py
@@ -0,0 +1,9 @@
+deposit = float(input())
+terms = int(input())
+percent = float(input())
+percent_decimal = percent / 100
+
+interest = deposit * percent_decimal
+interest_per_month = interest / 12
+total = deposit + terms * interest_per_month
+print(f"{total:.2f}")
