3164672179c86852a81b4e087533e6a25189baa3
Yulver <yulver.musa@gmail.com>
2025-08-18 20:17:26 +0200

    Added new comments.

--- python_foundations/2.02_radians_to_degrees.py
+++ python_foundations/2.02_radians_to_degrees.py
@@ -1,3 +1,8 @@
+# Deposit calculator:
+# Formula: sum = deposit + months * ((deposit * annual_interest) / 12).
+# Input: deposit, months, annual interest %.
+# Output: final amount.
+
 from math import pi
 radians = float(input())
 
