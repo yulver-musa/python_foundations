ef3688ba3257c2068ad2fe9a86a23640f374fcb2
Yulver <yulver.musa@gmail.com>
2025-08-21 18:51:06 +0200

    Added new file.

--- python_foundations/4.01_sum_seconds.py
+++ python_foundations/4.01_sum_seconds.py
@@ -1,3 +1,13 @@
+# Three athletes finish in a certain number of seconds
+# (between 1 and 50).
+# Write a program that:
+#  - Reads the finishing times of the three athletes in seconds,
+#  entered by the user.
+#  - Calculates their total time and displays it in the format
+#  "minutes:seconds".
+#  - The seconds must always be shown with a leading zero
+#    (e.g., 2 → "02", 7 → "07", 35 → "35").
+
 import math
 
 time_first = int(input())
